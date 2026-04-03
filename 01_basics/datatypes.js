60d19242632dd75ecd84c9aa814b72b5fe10b37a
Pranav Kharat <136214358+pranavkharat-404@users.noreply.github.com>
2026-04-03 20:42:16 +0000

    changes done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,4 @@
+let name = "pranav"
+let age = 19
+let isLoggedIn = true
+console.table([name, age, isLoggedIn])
---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+const rollNo = 43
+let studentName = "Pranav"
+var studentPass = "292005"
+accountCity = "nagpur"
+//rollNo = 10; // not allowed
+console.log(rollNo);
+
+studentName = "Vasudha"
+studentPass = "142006"
+accountCity = "Hyderabad"
+
+console.table([studentName, studentPass, accountCity])
