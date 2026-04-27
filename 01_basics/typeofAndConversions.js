b0b89cce4835d529c7a183ebe125ca8f71b2c5e1
Pranav Kharat <136214358+pranavkharat-404@users.noreply.github.com>
2026-04-27 05:06:55 +0000

    conversions confusion

---
+++ 01_basics/typeofAndConversions.js
@@ -0,0 +1,13 @@
+let score = "20"
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+let isLoggedin = 1
+let booleanisLoggedin = Boolean(isLoggedin)
+console.log(booleanisLoggedin)
+
+let notLoggedin = 0
+let booleannotLoggedin = Boolean(notLoggedin)
+console.log(booleannotLoggedin)
