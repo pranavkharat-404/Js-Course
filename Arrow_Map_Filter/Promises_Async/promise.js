f2c1e44f6f6cd72f527664f31254d111173506e8
Pranav Kharat <136214358+pranavkharat-404@users.noreply.github.com>
2026-06-03 08:54:16 +0000

    Promises and async await

--- Arrow_Map_Filter/Promises_Async/promise.js
+++
@@ -1,8 +0,0 @@
-function setTimeoutPromisified(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-function callback(){
-    console.log("Hi there");
-}
-
-setTimeoutPromisified(5000).then(callback);
